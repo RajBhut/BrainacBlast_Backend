797f379cbb965b0e4d9563aecfa22b446608d61d
RajBhut214 <127262686+RajBhut@users.noreply.github.com>
2024-05-16 11:55:05 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -114,7 +114,7 @@ app.post('/quizzes', async (req, res) => {
       if (i !== questions.length - 1) query += ', ';
 
     }
-    
+    response.end();
 
 
     const newQuiz = await pool.query(query);
